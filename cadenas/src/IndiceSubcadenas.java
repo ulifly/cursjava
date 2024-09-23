a059c72a96e41a3ef8e2914d051915fb49dfa041
Ulifly <ulifly@hotmail.com>
2024-09-23 10:10:22 -0600

    update

---
+++ cadenas/src/IndiceSubcadenas.java
@@ -0,0 +1,8 @@
+public class IndiceSubcadenas {
+    public static void main(String[] args) {
+        //Buscar subcadenas
+        //indexOff - Devuelve el indice de la primera aparicion de la suvcadena
+        var cadena1 = "Bonjour pedazo de zoquertes";
+        // subcadena a buscar "pedazo"
+    }
+}
